fc5538da559a416d354538790e0ac57ab882ab34
Giuseppe Di Mauro <azrafe7@gmail.com>
2024-03-16 03:25:59 +0100

    using playwright to generate pdf...
    and yes I'm on Upwork (Giuseppe D)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from jinja2 import Environment, PackageLoader, select_autoescape
 from jinja2.loaders import FileSystemLoader
+from playwright.sync_api import ViewportSize, sync_playwright
 
 import pdfkit
 
@@ -23,6 +24,9 @@ def render_template():
         summaryText=text,
     )
 
+    path_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+    config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
+
     # Open a file in write mode (w)
     with open("out/gen.html", "w") as file:
         # Write the string to the file
@@ -41,12 +45,30 @@ def render_template():
             options = {'enable-local-file-access': None}
 
             pdfkit.from_string(
-                genString, 'out/gen.pdf', options=options)
+                genString, 'out/gen.pdf', options=options, configuration=config)
 
             # output = r.to_pdf()
 
             print("pdf generated")
 
+
+    with sync_playwright() as p:
+
+        headless_browser = True
+        print("Launching " + ("Headless " if headless_browser else "") + "Browser...")
+
+        browser = p.chromium.launch(headless=headless_browser)
+        ua = "Mozilla/5.0 (Linux; Android 8.0.0; MI 6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.101 Mobile Safari/537.36"
+        context = browser.new_context(user_agent=ua)
+        page = context.new_page()
+        page.set_content(genString)
+        
+        # breakpoint()
+        
+        page.emulate_media(media="screen")
+        pw_pdf_file = "out/gen_pw.pdf"
+        page.pdf(path=pw_pdf_file, print_background=True, format="A4", scale=0.6)
+        print(f"{pw_pdf_file} generated")
 
 if __name__ == "__main__":
     render_template()
